881c10b60875446195ca4dc602c79b47959b12f2
Simmon Cheng <594298131@qq.com>
2014-01-10 23:12:48 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <ctime>
+
+//#define _DEBUG_
+
+using namespace std;
+
+
+vector<string>FileLineVector1;
+vector<string>FileLineVector2;
+vector<wchar_t>FileLineVectorInt1;
+vector<wchar_t>FileLineVectorInt2;
+map<string,int> mapStringToInt;
+string intToString[30000];
+int mapSize = 0;
+//map<string,int> mapStringToInt2;
+
